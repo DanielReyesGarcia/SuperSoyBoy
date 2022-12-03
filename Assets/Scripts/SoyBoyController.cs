84d5ba57cde2b5046e1716ab34d1041d311c659c
Daniel Reyes <danielreyesg94@gmail.com>
2022-12-03 00:23:57 -0500

    Chapter 13 - Added sounds and music

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -19,6 +19,10 @@ public class SoyBoyController : MonoBehaviour
     private float jumpDuration;
     public float airAccel = 3f;
     public float jump = 14f;
+    public AudioClip runClip;
+    public AudioClip jumpClip;
+    public AudioClip slideClip;
+    private AudioSource audioSource;
 
     void Awake()
     {
@@ -27,6 +31,7 @@ public class SoyBoyController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         width = GetComponent<Collider2D>().bounds.extents.x + 0.1f;
         height = GetComponent<Collider2D>().bounds.extents.y + 0.2f;
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Start()
@@ -80,8 +85,13 @@ public class SoyBoyController : MonoBehaviour
             if (input.y > 0f)
             {
                 isJumping = true;
+                PlayAudioClip(jumpClip);
             }
             animator.SetBool("IsOnWall", false);
+            if (input.x < 0f || input.x > 0f)
+            {
+                PlayAudioClip(runClip);
+            }
         }
 
         if (jumpDuration > jumpDurationThreshold) input.y = 0f;
@@ -135,6 +145,7 @@ public class SoyBoyController : MonoBehaviour
             * 0.75f, rb.velocity.y);
             animator.SetBool("IsOnWall", false);
             animator.SetBool("IsJumping", true);
+            PlayAudioClip(jumpClip);
         }
         else if (!IsWallToLeftOrRight())
         {
@@ -144,6 +155,7 @@ public class SoyBoyController : MonoBehaviour
         if (IsWallToLeftOrRight() && !PlayerIsOnGround())
         {
             animator.SetBool("IsOnWall", true);
+            PlayAudioClip(slideClip);
         }
 
         if (isJumping && jumpDuration < jumpDurationThreshold)
@@ -203,6 +215,14 @@ public class SoyBoyController : MonoBehaviour
         else
         {
             return 0;
+        }
+    }
+
+    void PlayAudioClip(AudioClip clip)
+    {
+        if (audioSource != null && clip != null)
+        {
+            if (!audioSource.isPlaying) audioSource.PlayOneShot(clip);
         }
     }
 }
